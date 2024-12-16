b4c0619c1ab66e1d526d3db2d4468ba6f8f2409c
alnandoh <alnandoharsono@gmail.com>
2024-12-16 21:19:40 +0700

    fix: update web path routing

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -26,7 +26,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('admin')
+            ->path('user')
             ->login()
             ->registration(Register::class)
             ->colors([
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect('/admin/login');
+    return redirect('/user/login');
 });
